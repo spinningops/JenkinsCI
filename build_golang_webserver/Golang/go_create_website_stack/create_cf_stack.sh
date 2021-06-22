bdd68241fd450789588d80e7977a2dfa38ed0355
Kobi <kobi700@gmail.com>
2021-06-22 16:55:04 +0300

    fix path

--- build_golang_webserver/Golang/go_create_website_stack/create_cf_stack.sh
+++ build_golang_webserver/Golang/go_create_website_stack/create_cf_stack.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-aws cloudformation create-stack --stack-name go-websample --template-body file://cf_create_ec2_go_template.yaml --parameters ParameterKey=KeyName,ParameterValue=jenkins-node
+aws cloudformation create-stack --stack-name go-websample --template-body file://build_golang_webserver/Golang/go_create_website_stack/cf_create_ec2_go_template.yaml --parameters ParameterKey=KeyName,ParameterValue=jenkins-node
